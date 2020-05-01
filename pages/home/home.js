66a76593847647d898d7f718fcd0fb24c7d8a733
ZH <ZH@zh.com>
2020-05-02 00:25:00 +0800

    第一次学习

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,30 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Coderwhy',
+    age: 18,
+    students: [
+      {id: 110, name : '郑豪', age: 24},
+      {id: 111, name : '万吉鑫', age: 25},
+      {id: 112, name : '马恩权', age: 26},
+      {id: 113, name : '张炫', age: 27}
+    ],
+    counter: 0
   },
+  handleBtnClick(){
+    // 1.错误做法：界面是不会刷新的
+    // this.data.counter +=1
+    // console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
+
 })
